5accfd480aace8e46a024402486e8c86026f5b64
Hrithin <100468394+hrithinnnn@users.noreply.github.com>
2023-07-09 22:53:29 +0530

    bug fixes

--- backend-new/index.js
+++ backend-new/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const data = require('../backend/assets/schema/formdata');
+const data = require('../backend-new/assets/schema/formdata');
 const app = express();
 const jsonParser = bodyParser.json();
 
